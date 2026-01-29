7d1da9408c3b6ff94cda0c241a452843ce5ba713
tomJacquemart <94608060+Motazerty@users.noreply.github.com>
2026-01-29 10:54:44 +0100

    Delete frontend directory
    Signed-off-by: tomJacquemart <94608060+Motazerty@users.noreply.github.com>

--- frontend/src/pages/About.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import { t } from '../utils/i18n';
-
-const About: React.FC = () => (
-  <Box sx={{ padding: '100px 20px', fontSize: 24, textAlign: 'center' }}>{t('aboutUs')}</Box>
-);
-
-export default About;
--- frontend/src/utils/videoAccess.ts
+++
@@ -1,6 +0,0 @@
-// Utility to check if a user can access a video from a pack
-// Usage: canAccessVideo(packId, purchasedPackIds)
-
-export function canAccessVideo(packId: number, purchasedPackIds: number[]): boolean {
-  return purchasedPackIds.includes(packId);
-}
